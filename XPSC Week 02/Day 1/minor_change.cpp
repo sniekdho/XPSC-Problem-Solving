a482550ca1f648fcb27ac4954db796162059c367
sniekdho <38922339+sniekdho@users.noreply.github.com>
2024-11-03 23:40:57 +0600

    Add files via upload

---
+++ XPSC Week 02/Day 1/minor_change.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    string s,t;
+    cin>>s>>t;
+
+    int count = 0;
+
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i] != t[i])
+        {
+            count++;
+        }
+    }
+
+    cout<<count;
+    
+    return 0;
+}
