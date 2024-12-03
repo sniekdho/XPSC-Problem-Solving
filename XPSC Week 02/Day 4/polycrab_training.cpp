c5f3405566a2b29dbc3d7af25e7d3ffa280fdb94
sniekdho <38922339+sniekdho@users.noreply.github.com>
2024-12-04 01:14:05 +0800

    Add files via upload
    Added week 02 day 04 codes

---
+++ XPSC Week 02/Day 4/polycrab_training.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+
+    multiset<int> ml;
+
+    for(int i=1; i<=n; i++)
+    {
+        int x;
+        cin>>x;
+        ml.insert(x);
+    }
+
+    int ans = 0, problems = 1;
+    while(!ml.empty())
+    {
+        auto LB = ml.lower_bound(problems);
+        if(LB != ml.end())
+        {
+            ans++;
+            ml.erase(LB);
+        }
+        else
+        {
+            break;
+        }
+        problems++;
+    }
+
+    cout << ans << endl;
+    
+    return 0;
+}
---
+++ XPSC Week 02/Day 4/train_queries.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        map<int, set<int>> mp;
+
+        for(int i=1; i<=n; i++)
+        {
+            int x;
+            cin>>x;
+            mp[x].insert(i);
+        }
+
+        for(int i=1; i<=m; i++)
+        {
+            int l,r;
+            cin>>l>>r;
+
+            if((mp.find(l) == mp.end()) || (mp.find(r) == mp.end()))
+            {
+                cout <<"NO"<<endl;
+            }
+            else
+            {
+                int startingIndex, endingIndex;
+                startingIndex = *mp[l].begin();
+                endingIndex = *mp[r].rbegin();
+
+                if(startingIndex < endingIndex)
+                {
+                    cout << "YES"<<endl;
+                }
+                else
+                {
+                    cout<<"NO"<<endl;
+                }
+            }
+        }
+    }
+    
+    
+    return 0;
+}
