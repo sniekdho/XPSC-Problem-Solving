d88b805645578107b41dde719d3a3582e0986a78
sniekdho <38922339+sniekdho@users.noreply.github.com>
2024-12-07 22:52:22 +0600

    Add files via upload
    Added week 02 day 06 codes

---
+++ XPSC Week 02/Day 6/berpizza.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    int n, customerNo=1;
+    cin>>n;
+
+    set<pair<int, int>> s;
+    multiset<pair<int, int>> ml;
+    vector<int> v;
+
+    for(int i=1; i<=n; i++)
+    {
+        int type;
+        cin >> type;
+
+        if(type == 1)
+        {
+            int money;
+            cin>> money;
+            s.insert({customerNo, money});
+            ml.insert({money, -customerNo});
+            customerNo++;
+        }
+        else if(type == 2)
+        {
+            int pos = s.begin()->first,money = s.begin()->second;
+            v.push_back(pos);
+            s.erase(s.begin());
+            ml.erase({money, -pos});
+        }
+        else 
+        {
+         int pos = -ml.rbegin()->second, money = ml.rbegin()->first;
+         v.push_back(pos);
+         ml.erase(--ml.end());
+         s.erase({ pos,money });
+      }
+
+    }
+    
+    for(auto value : v)
+    {
+        cout<< value <<" ";
+    }
+    cout << endl;
+
+    return 0;
+}
